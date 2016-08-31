9bcdeacb7274af4e2f8d292eb8050a2688b635f9
Terry O'Shea <terrykoshea@gmail.com>
2016-08-31 22:23:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A microservice to convert a url given by the user into a shorter version. 
+
+e.g.: ###examples will go here
---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var validUrl = require('valid-url');
+
+app.get('/', function(req, res) {
+    var fileName = path.join(__dirname, 'index.html');
+    res.sendFile(fileName, function (err) {
+        if (err) {
+            console.log(err);
+            res.status(err.status).end();
+        }
+    })
+});
+
+var shortened;
+var longurl;
+
+app.get('/:item', function(req, res) {
+    var item = req.params.item;
+    if (item == shortened) {
+        res.redirect(longurl);
+    }
+    else if (validUrl.isWebUri(item) != undefined) {
+        longurl = item;
+        shortened = Math.floor(Math.random()*1000) + 1;
+        res.json({
+            original_url: item,
+            short_url: 'comname/' + shortened
+        });
+    }
+    else {
+        res.json({
+            error: 'Wrong URL format!'
+        });
+    }
+});
+
+app.listen(process.env.PORT || 3500);
