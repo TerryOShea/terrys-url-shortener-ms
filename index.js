fd7f69a8e7fac1edbe601bf082201ccc14a7b01b
Terry O'Shea <terrykoshea@gmail.com>
2016-09-30 17:55:06 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,42 +1,86 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var validUrl = require('valid-url');
+const express = require('express');
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const validUrl = require('valid-url');
+const MongoClient = require('mongodb').MongoClient;
 
-app.get('/', function(req, res) {
-    var fileName = path.join(__dirname, 'index.html');
-    res.sendFile(fileName, function (err) {
-        if (err) {
-            console.log(err);
-            res.status(err.status).end();
-        }
+var currNum = 999;
+
+function numGen() {
+    currNum++;
+    return String('/' + currNum);
+};
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('view engine', 'ejs')
+
+MongoClient.connect('mongodb://terryoshea:pojkentalarinte@ds047146.mlab.com:47146/url-shortener', (err, db) => {
+	if (err) return console.log(err)
+	
+	app.get('/', function(req, res) {
+        var fileName = path.join(__dirname, 'index.html');
+        res.sendFile(fileName, function (err) {
+            if (err) {
+                console.log(err);
+                res.status(err.status).end();
+            }
+        });
     });
-});
+    
+    app.get('/:short', function(req, res) {
+        var shortUrl = req.params.short
 
-var shortened;
-var longurl;
-
-app.get('/:item', function(req, res) {
-    var item = req.params.item;
-    if (item == shortened) {
-        res.redirect(longurl);
-    }
-    else if (validUrl.isWebUri(item) != undefined) {
-        res.json({
-            original_url: item
+        db.collection('urls').findOne({
+            short_url: process.env.APP_URL + shortUrl
+        }, function(err, entry) {
+            if (err) throw err;
+            if (entry) {
+                res.redirect(entry.long_url)
+            }
+            else {
+                res.send('That shortened URL is not in our database!');
+            }
+        })
+    });
+	
+	app.post('/new_short', function(req, res) {
+        var longUrl = req.body.url;
+        
+        db.collection('urls').findOne({
+            long_url: longUrl
+        }, function(err, entry) {
+            if (err) throw err;
+            if (entry) {
+                res.send({
+                    long_url: entry.long_url, 
+                    short_url: entry.short_url
+                })
+            }
+            else {
+                if (validUrl.isWebUri(longUrl) != undefined) {
+                    var urlInfo = {
+                        long_url: longUrl, 
+                        short_url: process.env.APP_URL + numGen()
+                    };
+                    db.collection('urls').save(urlInfo, (err, result) => {
+    		            if (err) return console.log(err);
+    		            else {
+    		                res.json(urlInfo);
+    		            }
+                    })
+                }
+                else {
+                    res.json({
+                        error: 'Wrong URL format!'
+                    });
+                }
+            }
         });
-        longurl = item;
-        shortened = (Math.floor(Math.random()*1000) + 1).toString();
-        /*res.json({
-            original_url: item,
-            //short_url: 'https://terrys-url-shortener.herokuapp.com/' + shortened
-        });*/
-    }
-    else {
-        res.json({
-            error: 'Wrong URL format!'
-        });
-    }
+	});
+	
+	app.listen(process.env.PORT || 3500, () => {
+		console.log('listening on 3500');
+	});
 });
-
-app.listen(process.env.PORT || 3500);
